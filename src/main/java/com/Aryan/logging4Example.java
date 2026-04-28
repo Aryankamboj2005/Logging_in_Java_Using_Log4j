bbdd188dff59ec55b041e6962cea06fcfce884b7
Aryan <Aryankamboj2005@gmail.com>
2026-04-28 16:49:46 +0530

    initilizing the logger

--- src/main/java/com/Aryan/logging4Example.java
+++ src/main/java/com/Aryan/logging4Example.java
@@ -1,5 +1,11 @@
 package com.Aryan;
 
-public class logging4Example {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class logging4Example {
+	
+	// initilizing the logger 
+    private static final Logger logger = LogManager.getLogger(logging4Example.class);
+    
 }
