0cc668acb64bef071b7a5f3035d1a1dfb9c22687
Aryan <Aryankamboj2005@gmail.com>
2026-04-28 19:27:30 +0530

    writing log info to the file

--- src/main/java/com/Aryan/logging4Example.java
+++ src/main/java/com/Aryan/logging4Example.java
@@ -4,8 +4,34 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class logging4Example {
-	
-	// initilizing the logger 
+
+    // initilizing the logger
     private static final Logger logger = LogManager.getLogger(logging4Example.class);
-    
+
+    /**
+     * LOG4J PATTERN REFERENCE: %d - %c -[%M] -(%p) -%m -%n
+     * --------------------------------------------------
+     * %d  -> Date/Time of the log
+     * %c  -> Class Name (Category)
+     * %M  -> Method Name
+     * %p  -> Log Level (Priority)
+     * %m  -> The Log Message
+     * %n  -> Newline
+     */
+
+    public static void main(String[] args) {
+
+        process();
+    }
+
+    public static void process() {
+        // just practicing
+
+        logger.trace("tracing the mmessage");
+        logger.debug("debuged message ");
+        logger.info("from the info part");
+        logger.warn("from the warn part");
+        logger.error("from the error part");
+        logger.fatal("from teh fattal method");
+    }
 }
